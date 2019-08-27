d0cbb8bfc4136d2e11a2ebd8bae4992db738b11f
Columbus <sanuhcol@hotmail.com>
2019-08-27 21:16:40 +0100

    the third commit

--- src/main/java/Gitting.java
+++ src/main/java/Gitting.java
@@ -2,5 +2,6 @@ public class Gitting {
     public static void main(String[] args) {
         System.out.println("From the main method");
         // Another comment
+        //the therd branch
     }
 }
